363dfebb8430f4aefb9cd162c35aca107e431d48
liang <chengliang1987@icloud.com>
2018-11-22 14:00:19 +0800

    IntelliJ

---
+++ WhatIf.java
@@ -0,0 +1,15 @@
+public class WhatIf
+{
+    public static void main( String[] args)
+    {
+        int people = 20;
+        int cats = 30;
+        int dogs = 15;
+
+        if (people < cats)
+        {
+            System.out.println("Too many cats! The world is doomed.")
+        }
+
+    }
+}
